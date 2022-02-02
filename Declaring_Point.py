3dd99381fe24173eee044a4fd11380e317ade506
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-01 19:44:01 -0800

    Add files via upload

---
+++ Declaring_Point.py
@@ -0,0 +1,30 @@
+"""
+LESSON: 3.1 - Lines
+TECHNIQUE 2: Declaring Points
+DEMO
+"""
+
+import pygame
+pygame.init()
+
+window = pygame.display.set_mode([400, 400])
+window.fill((255, 255, 255))
+
+# Declare points
+top_left = (100, 100)
+top_right = (300, 100)
+bottom_left = (100, 300)
+bottom_right = (300, 300)
+
+# Draw lines
+pygame.draw.line(window, (0, 0, 0), top_left, top_right)
+pygame.draw.line(window, (0, 0, 0), top_left, bottom_left)
+pygame.draw.line(window, (0, 0, 0), top_right, bottom_right)
+pygame.draw.line(window, (0, 0, 0), bottom_left, bottom_right)
+pygame.draw.line(window, (0, 0, 0), top_left, bottom_right)
+pygame.draw.line(window, (0, 0, 0), bottom_left, top_right)
+
+
+
+pygame.display.flip()
+pygame.time.wait(5000)
