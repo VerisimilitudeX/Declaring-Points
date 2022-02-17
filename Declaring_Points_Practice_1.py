76602df6655cc7f711fb390195a01fd57ea8f3c3
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-17 13:19:55 -0800

    Update Declaring_Points_Practice_1.py

--- Declaring_Points_Practice_1.py
+++ Declaring_Points_Practice_1.py
@@ -4,18 +4,15 @@ pygame.init()
 window = pygame.display.set_mode([400, 400])
 window.fill((255, 255, 255))
 
-# Declare points
 top_left = (0, 200)
 top_right = (400, 200)
 bottom_left = (200, 0)
 bottom_right = (200, 400)
 
-# Draw line
 pygame.draw.line(window, (173, 216, 35), top_left, bottom_right, 2)
 pygame.draw.line(window, (173, 216, 35), bottom_left, top_right, 2)
 pygame.draw.line(window, (173, 216, 35), top_right, bottom_right, 2)
 pygame.draw.line(window, (173, 216, 35), top_left, bottom_left, 2)
-
 
 pygame.display.flip()
 pygame.time.wait(5000)
