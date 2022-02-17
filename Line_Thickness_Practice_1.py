2a9a6225bc91d96a17aa2f44d44e4286ad416d6b
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-17 13:20:39 -0800

    Update Line_Thickness_Practice_1.py

--- Line_Thickness_Practice_1.py
+++ Line_Thickness_Practice_1.py
@@ -4,14 +4,12 @@ pygame.init()
 window = pygame.display.set_mode([400, 400])
 window.fill((255, 255, 255))
 
-# Declare points
 point_1 = (0, 200)
 point_2 = (100, 200)
 point_3 = (200, 200)
 point_4 = (300, 200)
 point_5 = (400, 200)
 
-# Draw lines
 pygame.draw.line(window, (212, 255, 255), point_1, point_2, 7)
 pygame.draw.line(window, (214, 255, 255), point_2, point_3, 8)
 pygame.draw.line(window, (193, 255, 255), point_3, point_4, 9)
